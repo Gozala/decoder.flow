1e2e43bfb94c472b8a89069706fab18b8c060a3b
Irakli Gozalishvili <rfobic@gmail.com>
2017-08-01 00:33:11 -0700

    Update tests for either.

--- test/Either.js
+++ test/Either.js
@@ -6,36 +6,42 @@ import test from "blue-tape"
 import testDecode from "./Decoder/Decoder"
 
 test("Decoder.either", async test => {
-  const stringOrInt = Decoder.either(Decoder.String, Decoder.Integer)
+  const decoder = Decoder.either(Decoder.String, Decoder.annul(""))
 
-  test.deepEqual(stringOrInt, {
+  test.deepEqual(decoder, {
     type: "Either",
-    either: [{ type: "String" }, { type: "Integer" }]
+    either: [{ type: "String" }, { type: "Null", Null: "" }]
   })
 })
 
 testDecode(
   {
-    "Decoder.either(Decoder.String, Decoder.Integer)": Decoder.either(
+    'Decoder.either(Decoder.String, Decoder.annul(""))': Decoder.either(
       Decoder.String,
-      Decoder.Integer
+      Decoder.annul("")
     ),
-    '{type:"Either",either:[{type:"String"},{type:"Integer"}]}': ({
+    '{type:"Either",either:[{type:"String"},{type:"Null",Null:""}]}': ({
       type: "Either",
-      either: [{ type: "String" }, { type: "Integer" }]
+      either: [{ type: "String" }, { type: "Null", Null: "" }]
     }: any)
   },
   {
+    null: Result.ok(""),
     '""': Result.ok(""),
-    "0": Result.ok(0),
     'new String("")': Result.ok(""),
     '"hello"': Result.ok("hello"),
-    'new String("Hello")': Result.ok("Hello"),
-    "-15": Result.ok(-15),
-    "15": Result.ok(15)
+    'new String("Hello")': Result.ok("Hello")
   },
   key =>
     Result.error(
-      `Ran into the following problems:\n\nExpecting a String but instead got: \`${key}\`\nExpecting an Integer but instead got: \`${key}\``
+      `Ran into the following problems:\n\nExpecting a String but instead got: \`${key}\`\nExpecting a null but instead got: \`${key}\``
     )
 )
+
+test("Decoder.either from readme", async test => {
+  const badName = Decoder.either(Decoder.String, Decoder.annul(""))
+  test.deepEqual(
+    Decoder.decode(Decoder.array(badName), ["alice", "bob", null, "chuck"]),
+    Result.ok(["alice", "bob", "", "chuck"])
+  )
+})
