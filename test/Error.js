0f20fee9343a2dc703f04b99c1c4de39c44000ed
Irakli Gozalishvili <rfobic@gmail.com>
2017-08-01 00:17:07 -0700

    Add tests for Decoder.error.

--- test/Error.js
+++ test/Error.js
@@ -21,3 +21,41 @@ testDecode(
   {},
   _ => Result.error(`Boom!`)
 )
+
+test("Decoder.error from readme", async test => {
+  test.deepEqual(
+    Decoder.decode(Decoder.error("Boom!"), true).format(x => x.message),
+    Result.error("Boom!")
+  )
+  test.deepEqual(
+    Decoder.decode(Decoder.error("Boom!"), [1, 2, 3]).format(x => x.message),
+    Result.error("Boom!")
+  )
+  test.deepEqual(
+    Decoder.decode(Decoder.error("Boom!"), "hello").format(x => x.message),
+    Result.error("Boom!")
+  )
+
+  const phone = Decoder.either(
+    Decoder.field("cell", Decoder.String),
+    Decoder.field("home", Decoder.String),
+    Decoder.error("No phone number")
+  )
+
+  test.deepEqual(
+    Decoder.decode(phone, { cell: "415-5588-0000", home: "415-8855-0000" }),
+    Result.ok("415-5588-0000")
+  )
+  test.deepEqual(
+    Decoder.decode(phone, { home: "415-8855-0000" }),
+    Result.ok("415-8855-0000")
+  )
+  test.deepEqual(
+    Decoder.decode(phone, {}).format(x => x.message),
+    Result.error(`Ran into the following problems:
+
+Expecting an object with a field named 'cell' but instead got: \`{}\`
+Expecting an object with a field named 'home' but instead got: \`{}\`
+No phone number`)
+  )
+})
